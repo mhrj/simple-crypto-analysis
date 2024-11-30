8af25098213697a4260ced30c3f93f97664db19e
Mahdi Payanbenam <mahdi333benam@gmail.com>
2024-11-30 20:57:19 +0330

    fixed helpers import

--- backend/home/fan_chart_data.py
+++ backend/home/fan_chart_data.py
@@ -1,5 +1,9 @@
+import os
+import sys
 import pyRserve
 from typing import Dict, List
+parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+sys.path.append(parent_dir)
 import helpers
 from constants import CRYPTOCOMPARE_BASE_URL
 
